0d5f7d553b7509563003044c0c64404bd52d8892
Eamon Powell <eamonpowell@outlook.com>
2019-08-06 07:46:29 +1000

    wahoo: Skip SurfaceFlinger propagating backpressure to apps & enable hardware acceleration
    Change-Id: I38c03fe09824f613d673a216b6be6d880fb1c3ad

--- device.mk
+++ device.mk
@@ -738,6 +738,11 @@ PRODUCT_PRODUCT_PROPERTIES += \
     ro.opa.eligible_device=true \
     ro.gfx.angle.supported=true
 
+# SurfaceFlinger and Hardware Acceleration Tweaks
+PRODUCT_PROPERTY_OVERRIDES += \
+   debug.sf.disable_backpressure=1 \
+   video.accelerate.hw=1
+
 # MiFare Permissions file
 PRODUCT_COPY_FILES += \
       frameworks/native/data/etc/com.nxp.mifare.xml:system/etc/permissions/com.nxp.mifare.xml
