6885d53a224533481fd51001c0849427758a7750
Josh Chasky <jchasky@gmail.com>
2019-09-27 20:13:18 -0400

    Compile IMS objects from AOSP

--- device.mk
+++ device.mk
@@ -717,3 +717,9 @@ PRODUCT_COPY_FILES += \
 # EUICC feature
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml
+
+# IMS
+PRODUCT_PACKAGES += \
+    com.android.ims.rcsmanager \
+    PresencePolling \
+    RcsService
