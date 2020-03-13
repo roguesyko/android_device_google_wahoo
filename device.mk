a33593f1478710cf4cf670d652e801f3ace1ef95
Eamon Powell <eamonpowell@outlook.com>
2020-03-14 10:49:12 +1100

    wahoo: Build IPACM_cfg.xml
    This doesn't need to be manually copied as the one generated by product packages is identical.
    
    Change-Id: I98823229cc6a013bc96920df9b068470b3a7946d

--- device.mk
+++ device.mk
@@ -307,10 +307,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi_concurrency_cfg.txt:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wifi_concurrency_cfg.txt
 
-#ipacm configuration files
-PRODUCT_COPY_FILES += \
-    hardware/qcom/data/ipacfg-mgr/msm8998/ipacm/src/IPACM_cfg.xml:$(TARGET_COPY_OUT_VENDOR)/etc/IPACM_cfg.xml
-
 PRODUCT_PACKAGES += \
     hwcomposer.msm8998 \
     android.hardware.graphics.composer@2.1-impl:64 \
@@ -638,6 +634,10 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_PACKAGES += \
     ipacm
+
+# ipacm configuration files
+PRODUCT_PACKAGES += \
+    IPACM_cfg.xml
 
 #Set default CDMA subscription to RUIM
 PRODUCT_PROPERTY_OVERRIDES += \
